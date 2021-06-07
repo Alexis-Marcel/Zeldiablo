d3aef475cd93ed77c4f1854f1bc7186c8501936e
Theo Faedo <theo.faedo@gmail.com>
2021-06-07 15:41:26 +0200

    nvx moteur

---
+++ projet_zeldiablo/jeu/ZeldiabloDessin.java
@@ -0,0 +1,17 @@
+package jeu;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import moteurJeu.moteur.*;
+
+public class ZeldiabloDessin implements DessinAbstract{
+
+    @Override
+    public void dessiner(BufferedImage image) {
+        Graphics g = image.getGraphics();
+        
+    }
+    
+    
+}
