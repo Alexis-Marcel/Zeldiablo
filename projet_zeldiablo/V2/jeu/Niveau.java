2397009b0964b3842ed9bc130bd79339d2553c41
Alexis Marcel <AlexisMarcel55@gmail.com>
2021-06-08 11:55:16 +0200

    --

--- projet_zeldiablo/V2/jeu/Niveau.java
+++ projet_zeldiablo/V2/jeu/Niveau.java
@@ -1,5 +1,8 @@
 package V2.jeu;
 
+//import
+
+import Personnage.*;
 /**
  * Niveau
  */
@@ -11,8 +14,25 @@ public class Niveau {
      */
     private Labyrinthe laby;
 
+    /**
+     * Liste des monstres présent sur le labyrinthe
+     */
+    private ArrayList<Monstre> listeMonstre;
+
+    /**
+     * Aventurier qui va etre controler par l'utilisateur
+     */
+    private Aventurier hero;
+
     public Niveau(String src){
+
+        this.laby = new Labyrinthe(src);
+        this.listeMonstre = new ArrayList<Monstre>();
         
     }
+
+    public void deplacerAventurier()
+
+    
     
 } 
